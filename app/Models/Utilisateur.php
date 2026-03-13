807bf83ff7ac30e46102d8174ee519881796ced1
unknown <koneabou669@gmail.com>
2026-03-13 09:18:20 +0000

    Crud bibilothèque users

---
+++ app/Models/Utilisateur.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Utilisateur extends Model
+{
+    //
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'type',
+        'matricule',
+        'historique_emprunts'
+    ];
+}
